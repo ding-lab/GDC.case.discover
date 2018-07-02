22229e6fadc26ef187456ed8eb42de396be1b457
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-07-02 12:14:48 -0500

    Adding exit value error checking to get_sample.sh
    * note that the other scripts don't have this yet, may want to incorporate to the rest of them

--- get_sample.sh
+++ get_sample.sh
@@ -12,7 +12,7 @@
 if [ "$#" -ne 1 ]; then
     >&2 echo Error: Wrong number of arguments
     >&2 echo Usage: get_sample.sh CASE 
-    exit 0
+    exit 1
 fi
 
 CASE=$1
@@ -21,8 +21,25 @@ if [ -z $GDC_TOKEN ]; then
     >&2 GDC_TOKEN environment variable not defined.  Quitting.
     exit 1
 fi
- 
 
+# Called after running scripts to catch fatal (exit 1) errors
+# works with piped calls ( S1 | S2 | S3 > OUT )
+# Usage:
+#   bash script.sh DATA | python script.py > $OUT
+#   test_exit_status # Calls exit V if exit value of V of either call not 0
+function test_exit_status {
+    # Evaluate return value for chain of pipes; see https://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
+    # exit code 137 is fatal error signal 9: http://tldp.org/LDP/abs/html/exitcodes.html
+
+    rcs=${PIPESTATUS[*]};
+    for rc in ${rcs}; do
+        if [[ $rc != 0 ]]; then
+            >&2 echo Fatal error.  Exiting
+            exit $rc;
+        fi;
+    done
+}
+ 
 function sample_from_case_query {
 CASE=$1 # E.g C3L-00004
 cat <<EOF
@@ -52,10 +69,13 @@ Q=$(sample_from_case_query $CASE)
 
 >&2 echo QUERY: $Q
 
-#R=$(echo $Q | queryGDC -r -v -t $TOKEN -)
+# The actual call to queryGDC script
 R=$(echo $Q | $QUERYGDC -r -v -)
+test_exit_status
+
 
 echo $R | jq -r '.data.sample[] | "\(.submitter_id)\t\(.id)\t\(.sample_type)"' > $OUT
+test_exit_status
 
 echo Written to $OUT
 printf "\n"
